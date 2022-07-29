953f295e7d64a514cd17e6ece38874a834e11ce6
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2022-07-29 08:43:50 +0600

    Episode 10 done

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Data.FileManager;
 using Blog.Data.Repository;
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +13,13 @@ namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
-        private IRepository _repo;
-        public HomeController(IRepository repo)
+        private readonly IRepository _repo;
+        private readonly IFileManager _fileManager;
+
+        public HomeController(IRepository repo, IFileManager fileManager)
         {
             _repo = repo;
+            _fileManager = fileManager;
         }
 
         public IActionResult Index()
@@ -28,6 +32,14 @@ namespace Blog.Controllers
             var post = _repo.GetPost(id);
 
             return View(post);
+        }
+
+        //[HttpGet("[controller]/Image/{image}")]
+        [HttpGet("/Image/{image}")]
+        public IActionResult Image(string image)
+        {
+            var mime = image.Substring(image.LastIndexOf('.') + 1);
+            return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
         }
     }
 }
--- Blog/Data/FileManager/IFileManager.cs
+++ Blog/Data/FileManager/IFileManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Blog.Data.FileManager
 {
     public interface IFileManager
     {
+        FileStream ImageStream(string image);
         Task<string> SaveImage(IFormFile image);
     }
 }
